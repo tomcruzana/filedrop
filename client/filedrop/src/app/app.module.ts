269a4cc2ff1b81b5f5a890b06971d6bf68aa4f18
tomcruzana <tomcruzana@ymail.com>
2023-11-09 19:01:30 -0500

    implemented client's base upload logic

--- client/filedrop/src/app/app.module.ts
+++ client/filedrop/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageUploadComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
