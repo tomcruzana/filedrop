9a235ba801ce7b7a6c869024c378694aa5eaaceb
tomcruzana <tomcruzana@ymail.com>
2023-11-10 00:13:55 -0500

    implemented backend's base upload logic

---
+++ server/filedrop/src/main/java/com/example/demo/FiledropApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.service.FilesStorageService;
+
+import jakarta.annotation.Resource;
+
+@SpringBootApplication
+public class FiledropApplication implements CommandLineRunner{
+	
+	@Resource
+	FilesStorageService storageService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiledropApplication.class, args);
+	}
+	
+	@Override
+	  public void run(String... arg) throws Exception {
+//	    storageService.deleteAll();
+	    storageService.init();
+	  }
+
+}
